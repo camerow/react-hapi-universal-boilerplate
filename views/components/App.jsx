28fe7f472fc3e6be26db987b6d7d448dbe8f23e9
Will Cameron <cameron.will@gmail.com>
2015-12-23 13:53:25 -0800

    pull out components

--- views/components/App.jsx
+++ views/components/App.jsx
@@ -1,86 +1,9 @@
 import React, { Component } from "react";
-import Octicon from "react-octicon";
-import CopyToClipboard from 'react-copy-to-clipboard';
 import { format } from "../../lib/phoneNumber";
+import { InputArea, CopyArea } from "./";
 
 import "../../assets/style/foundation.min.css";
 import "../../assets/style/main.css";
-
-class CopyArea extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      copied: false
-    }
-  }
-  toAnchor(link) {
-    // console.log("ANCHOR", "<a href=" + link + ">Text Now</a>");
-    if (link !== "") {
-      this.setState({
-        show: "<a href=" + link + ">Text Now</a>"
-      })
-    }
-  }
-  render() {
-    const { link } = this.props;
-    return (
-      <div className="row align-center">
-        <div className="large-6 medium-10 small-12 column copy-region">
-          <CopyToClipboard text={ link }
-            onCopy={ () => this.setState({ copied: true }) }>
-            <button
-              className="copy-btn float-left"
-              data-tooltip={this.state.copied ? "Copied!" : "Click to copy to clipboard."}
-              onMouseLeave={ () => this.setState({ copied: false })}>
-              <Octicon style={{color: "white"}} name="clippy"></Octicon>
-            </button>
-          </CopyToClipboard>
-
-          <div className="link-container">
-            <code className="center-text">{ this.state.show || link }</code>
-          </div>
-
-          <p className="center-text format-options">
-            <a onClick={ () => this.setState({ show: null }) } className="format-options-item">URL </a>
-            <a onClick={ () => this.toAnchor(link) } className="format-options-item">Tag</a>
-          </p>
-        </div>
-      </div>
-    )
-  }
-}
-
-class InputArea extends Component {
-  render() {
-    const { formattedNumber } = this.props;
-    return (
-      <div className="row align-center">
-        <div className="small-12 medium-10 large-6 column center-text">
-
-          <input
-            placeholder="Phone Number"
-            value={ formattedNumber }
-            onChange={this.props.onNumberChange}
-            id="phone"
-            type="text"
-            label="input" />
-          <br />
-          <br />
-
-          <textarea
-            onChange={this.props.stringEncode}
-            placeholder="Message"
-            maxLength="160"
-            id="message"
-            type="text"
-            label="input">
-          </textarea>
-
-        </div>
-      </div>
-    )
-  }
-}
 
 class App extends Component {
   constructor(props) {
@@ -116,7 +39,7 @@ class App extends Component {
     if(phonenumber) {
       url += "http://zwt.co/sms?t=" + phonenumber;
       if(encodedMessage) {
-        url += "&body=" + encodedMessage;
+        url += "&b=" + encodedMessage;
       }
     }
     return url;
---
+++ webpack.production.config.js
@@ -0,0 +1,22 @@
+// Webpack production config file
+module.exports = {
+  entry: './views/main.jsx',
+  output: {
+    path: __dirname + '/assets/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'google-libphonenumber': '../node_modules/google-libphonenumber/dist/libphonenumber.js'
+    },
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, exclude: /node_modules/, loaders: ["babel"] },
+      { test: /\.js$/, exclude: /node_modules/, loaders: ["babel"] },
+      { test: /\.css$/, loader: "style!css" },
+      { test   : /\.(otf|eot|svg|ttf|woff|woff2).*$/, loader : 'url-loader' }
+    ]
+  },
+};
